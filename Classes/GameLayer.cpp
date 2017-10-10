a29669378233ccc8a71321baeafef082df9786cd
SeitaHigashi <higashi110902@gmail.com>
2017-10-10 22:47:25 +0900

    ProjectStart

---
+++ Classes/GameLayer.cpp
@@ -0,0 +1,8 @@
+//
+//  GameLayer.cpp
+//  ShootIQ
+//
+//  Created by 東誠太 on 2017/10/10.
+//
+
+#include "GameLayer.hpp"
---
+++ Classes/GameLayer.hpp
@@ -0,0 +1,13 @@
+//
+//  GameLayer.hpp
+//  ShootIQ
+//
+//  Created by 東誠太 on 2017/10/10.
+//
+
+#ifndef GameLayer_hpp
+#define GameLayer_hpp
+
+#include <stdio.h>
+
+#endif /* GameLayer_hpp */

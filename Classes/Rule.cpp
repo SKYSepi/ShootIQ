6c9cd17100dbd7cc0351561f6ec9386619d03486
SeitaHigashi <higashi110902@gmail.com>
2017-10-21 03:02:14 +0900

    Complete?

---
+++ Classes/Rule.cpp
@@ -0,0 +1,8 @@
+//
+//  Rule.cpp
+//  ShootIQ
+//
+//  Created by 東誠太 on 2017/10/21.
+//
+
+#include "Rule.hpp"
---
+++ Classes/Rule.hpp
@@ -0,0 +1,13 @@
+//
+//  Rule.hpp
+//  ShootIQ
+//
+//  Created by 東誠太 on 2017/10/21.
+//
+
+#ifndef Rule_hpp
+#define Rule_hpp
+
+#include <stdio.h>
+
+#endif /* Rule_hpp */

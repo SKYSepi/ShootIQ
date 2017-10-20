5a9e0b596f67d49450790da3187bd4058b21731a
SeitaHigashi <higashi110902@gmail.com>
2017-10-20 10:29:32 +0900

    ResultLayer add

---
+++ Classes/ResultLayer.cpp
@@ -0,0 +1,8 @@
+//
+//  ResultLayer.cpp
+//  ShootIQ
+//
+//  Created by 東誠太 on 2017/10/20.
+//
+
+#include "ResultLayer.hpp"
---
+++ Classes/ResultLayer.hpp
@@ -0,0 +1,13 @@
+//
+//  ResultLayer.hpp
+//  ShootIQ
+//
+//  Created by 東誠太 on 2017/10/20.
+//
+
+#ifndef ResultLayer_hpp
+#define ResultLayer_hpp
+
+#include <stdio.h>
+
+#endif /* ResultLayer_hpp */
